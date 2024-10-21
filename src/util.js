bdb81e33babe2c2d1984b0c6fce5fbeeb9da6ab4
Abhijith Acharya <abhijith.acharya@epsilon.com>
2024-10-21 22:19:11 +0530

    comfy store WIP

--- src/util.js
+++ src/util.js
@@ -26,9 +26,15 @@ const getStorageItem = (item) =>{
     return storageItem
 }
 
+const formatPrice = (price) =>{
+    const formattedPrice = new Intl.NumberFormat('en-US', {style:'currency', currency:'USD'}).format((price / 100).toFixed(2))
+    return formattedPrice;
+}
+
 export {
 allProductsUrl,
 singleProductUrl,
+formatPrice,
 setStorageItem,
 getStorageItem,
 getElement}
